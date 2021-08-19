3bf32dcd3f630ccf69ec6bab2d15be89de654dbd
g-NOTE-HST <jorge_mira@hst.com.br>
2021-08-19 03:25:16 -0300

    Algoritimo para Zero em date()

--- src/Paginas/DataHorario.js
+++ src/Paginas/DataHorario.js
@@ -5,15 +5,43 @@ export default function DataHorario(){
         let date = new Date()
         return date
     }
-    
+
+    const mesMDHM = 'month'
+    const diaMDHM = 'day'
+    const horaMDHM = 'hour'
+    const minutoMDHM = 'minute'
+    const segundoMDHM = 'second'
+    const zeroMDHM = (MDHM, prop) => {
+        let zero = '0' + prop
+        switch (MDHM) {
+            case 'month': 
+                return prop <= 9 ? zero : prop
+            case 'day': 
+                return prop <= 9 ? zero : prop
+            case 'hour': 
+                return prop <= 9 ? zero : prop    
+            case 'minute': 
+                return prop <= 9 ? zero : prop  
+            case 'second': 
+                return prop <= 9 ? zero : prop  
+
+            default:
+                break;
+        }
+    }
     return(
         <>
-
             <span>
-                Hoje {dh().getDate()}/{dh().getMonth()}/{dh().getFullYear()},
-                exatamente as <b>{dh().toLocaleTimeString()}</b>
+                Hoje {}
+                    {zeroMDHM(diaMDHM, dh().getDate())}/ 
+                    {zeroMDHM(mesMDHM, dh().getMonth())}/ 
+                    {dh().getFullYear()},
+                exatamente as <b>
+                    {zeroMDHM(horaMDHM, dh().getHours())}:
+                    {zeroMDHM(minutoMDHM, dh().getMinutes())}:
+                    {zeroMDHM(segundoMDHM, dh().getSeconds())}
+                </b>
             </span> 
-
         </>
     )
 }
